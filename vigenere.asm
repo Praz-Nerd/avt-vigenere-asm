641dd1db24330a255e1601417c4a935204962778
UciuIanis <uciuianis22@stud.ase.ro>
2026-01-14 20:53:46 +0200

    added more arguments to parse_args and refactored some jumps

--- vigenere.asm
+++ vigenere.asm
@@ -5,14 +5,24 @@ exit_dos MACRO
 	INT 21h
 ENDM
 
+close_file MACRO handle
+    xor AX, AX
+    mov AH, 3Eh ;file close function code
+    mov BX, handle ;load handler into BX
+    int 21h ;call interrupt
+ENDM
+
 .data
     psp_seg dw 0
     keyFileName db 128 dup(0)        
-    messageFileName db 128 dup(0) 
+    inputFileName db 128 dup(0)     
+    outputFileName db 128 dup(0) 
+    is_decryption DB 1 ;0 for encryption, 1 for decryption
 
     keySize dw ?                       
     chunkSize dw ?                     
-    fileHandle dw ?                    
+    inputFileHandle dw ?              
+    outputFileHandle dw ?              
 
     keyBuffer db 256 dup(?)           
     chunkBuffer db 512 dup(?)          
@@ -26,45 +36,63 @@ start:
 
     call parse_args
     cmp ax, 1
-    je endProcessing
-
+    jne skip_endProcessing1
+    jmp endProcessing
+skip_endProcessing1:
     lea ax, keyFileName
     push ax                ; pointer to file name
     lea ax, keyBuffer
-    push ax                ; [pointer to buffer
+    push ax                ; pointer to buffer
     mov ax, 256
     push ax                ; max buffer size
     call loadKeyFile
     cmp ax, 0
-    je endProcessing
+    jne skip_endProcessing2
+    jmp endProcessing
+skip_endProcessing2:
     mov keySize, ax
 
-    lea ax, messageFileName
-    push ax                ; pointer to file name
-    call openFileForReadWrite
+    ; open input file for reading
+    lea ax, inputFileName
+    push ax
+    call openFileForRead
     cmp ax, 0FFFFh
-    je endProcessing
-    mov fileHandle, ax
+    jne skip_endProcessing3
+    jmp endProcessing
+skip_endProcessing3:
+    mov inputFileHandle, ax
+
+    ; create output file for writing
+    lea ax, outputFileName
+    push ax
+    call createFileForWrite
+    cmp ax, 0FFFFh
+    jne skip_closeInputAndExit
+    jmp closeInputAndExit
+skip_closeInputAndExit:
+    mov outputFileHandle, ax
 
 processChunks:
-    ; read a chunk from the message file
+    ; read a chunk from the input file
     lea ax, chunkBuffer
     push ax                ; buffer offset
-    mov ax, fileHandle
+    mov ax, inputFileHandle
     push ax                ; file handle
     mov ax, 512
     push ax                ; chunk size
     call readChunk
     mov chunkSize, ax
     cmp ax, 0
-    je doneProcessing
+    jne skip_doneProcessing
+    jmp doneProcessing
+skip_doneProcessing:
 
     ;Encode/decode the chunk using Vigenère cipher
 
-    ; write the chunk back to the file
+    ; write the chunk to the output file
     lea ax, chunkBuffer
     push ax                ; buffer offset
-    mov ax, fileHandle
+    mov ax, outputFileHandle
     push ax                ; file handle
     mov ax, chunkSize
     push ax                ; chunk size
@@ -72,9 +100,18 @@ processChunks:
 
     jmp processChunks
 doneProcessing:
-    mov bx, fileHandle
-    mov ax, 3E00h
-    int 21h
+    ; close input file
+    mov bx, inputFileHandle
+    close_file bx
+    ; close output file
+    mov bx, outputFileHandle
+    close_file bx
+    jmp endProcessing
+
+closeInputAndExit:
+    mov bx, inputFileHandle
+    close_file bx
+
 endProcessing:
     exit_dos
 
@@ -112,24 +149,44 @@ fileKeyDone:
     ret 6
 loadKeyFile endp
 
-; open a file for read/write
+; open a file for reading
 ; in: [bp+4]=pointer to file name
-; out: AX = file handle
-openFileForReadWrite proc near
+; out: AX = file handle or 0FFFFh on error
+openFileForRead proc near
     push bp
     mov bp, sp
-    mov ax, 3D02h
+    mov ax, 3D00h
     mov dx, [bp+4]
     int 21h
-    jc openError
-    jmp openFileDone
+    jc openReadError
+    jmp openReadDone
 
-openError:
+openReadError:
     mov ax, 0FFFFh
-openFileDone:
+openReadDone:
     pop bp
     ret 2
-openFileForReadWrite endp
+openFileForRead endp
+
+; create a file for writing
+; in: [bp+4]=pointer to file name
+; out: AX = file handle or 0FFFFh on error
+createFileForWrite proc near
+    push bp
+    mov bp, sp
+    mov ah, 3Ch
+    xor cx, cx
+    mov dx, [bp+4]
+    int 21h
+    jc createWriteError
+    jmp createWriteDone
+
+createWriteError:
+    mov ax, 0FFFFh
+createWriteDone:
+    pop bp
+    ret 2
+createFileForWrite endp
 
 ; read a chunk from a file
 ; in: [bp+6]=buffer, [bp+4]=file handle, [bp+2]=chunk size
@@ -157,40 +214,29 @@ writeChunk proc near
     push bp
     mov bp, sp
 
-    mov ax, 4201h
-    mov bx, [bp+6]
-    mov cx, 0FFFFh
-    mov dx, [bp+4]
-    neg dx
-    int 21h
-
+    ; removed seek logic, just write at current position
     mov cx, [bp+4]
     mov dx, [bp+8]
     mov bx, [bp+6]
     mov ax, 4000h
     int 21h
 
-    mov ax, 4201h
-    mov bx, [bp+6]
-    xor cx, cx
-    mov dx, [bp+4]
-    int 21h
-
-writeChunkDone:
     pop bp
     ret 8
 writeChunk endp
 
 ; parse command line arguments
-; out: keyFileName = argument1, messageFileName = argument2
-; AX = 0 if args found, 1 if no args
+; out: keyFileName = arg1, inputFileName = arg2, outputFileName = arg3, is_decryption = arg4 (E/D)
+; AX = 0 if args found, 1 if not enough args or invalid mode
 parse_args proc near
     mov es, word ptr psp_seg
 
     xor cx, cx
     mov cl, es:[80h]
     cmp cl, 0
-    je no_args
+    jne skip_no_args
+    jmp no_args
+skip_no_args:
 
     mov si, 81h
 
@@ -207,7 +253,9 @@ start_arg1:
 parse_arg1:
     mov al, es:[si]
     cmp al, 0Dh     ; Carriage return
-    je end_of_line
+    jne skip_end_of_line1
+    jmp end_of_line
+skip_end_of_line1:
     cmp al, ' '
     je arg1_done
     mov [di], al
@@ -219,27 +267,91 @@ parse_arg1:
 arg1_done:
     mov byte ptr [di], 0
 
-skip_spaces:
+skip_spaces1:
     mov al, es:[si]
     cmp al, ' '
     jne start_arg2
     inc si
-    loop skip_spaces
+    loop skip_spaces1
     jmp end_of_line
 
 start_arg2:
-    mov di, offset messageFileName
+    mov di, offset inputFileName
 parse_arg2:
     mov al, es:[si]
     cmp al, 0Dh     ; Carriage return
+    jne skip_end_of_line2
+    jmp end_of_line
+skip_end_of_line2:
+    cmp al, ' '
     je arg2_done
     mov [di], al
     inc di
     inc si
     loop parse_arg2
+    jmp end_of_line
 
 arg2_done:
     mov byte ptr [di], 0
+
+skip_spaces2:
+    mov al, es:[si]
+    cmp al, ' '
+    jne start_arg3
+    inc si
+    loop skip_spaces2
+    jmp end_of_line
+
+start_arg3:
+    mov di, offset outputFileName
+parse_arg3:
+    mov al, es:[si]
+    cmp al, 0Dh     ; Carriage return
+    jne skip_end_of_line3
+    jmp end_of_line
+skip_end_of_line3:
+    cmp al, ' '
+    je arg3_done
+    mov [di], al
+    inc di
+    inc si
+    loop parse_arg3
+
+arg3_done:
+    mov byte ptr [di], 0
+
+skip_spaces3:
+    mov al, es:[si]
+    cmp al, ' '
+    jne start_arg4
+    inc si
+    loop skip_spaces3
+    jmp end_of_line
+
+start_arg4:
+    mov al, es:[si]
+    cmp al, 0Dh
+    jne skip_end_of_line4
+    jmp end_of_line
+skip_end_of_line4:
+    ; Accept only one char for mode
+    cmp al, 'E'
+    je set_encrypt
+    cmp al, 'e'
+    je set_encrypt
+    cmp al, 'D'
+    je set_decrypt
+    cmp al, 'd'
+    je set_decrypt
+    jmp end_of_line
+
+set_encrypt:
+    mov byte ptr [is_decryption], 0
+    mov ax, 0
+    ret 0
+
+set_decrypt:
+    mov byte ptr [is_decryption], 1
     mov ax, 0
     ret 0
 
@@ -249,7 +361,8 @@ end_of_line:
 
 no_args:
     mov byte ptr [keyFileName], 0
-    mov byte ptr [messageFileName], 0
+    mov byte ptr [inputFileName], 0
+    mov byte ptr [outputFileName], 0
     mov ax, 1
     ret 0
 parse_args endp
